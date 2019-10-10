1406d043bc4f750610ae1e5f40910837ec1baa64
AriesHoo@126.com <HT19930614>
2019-10-10 17:32:34 +0800

    Initial commit

---
+++ lib/module/new_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NewPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New Page'),
+      ),
+      body: Center(
+        child: Text('This my new page'),
+      ),
+    );
+  }
+}
